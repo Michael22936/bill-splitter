03c63e557066a34bbf10bf43cc94d73ba13fc171
Michael McDonnell <Michael.McDonnell067@gmail.com>
2020-01-15 11:32:12 -0600

    created basic bill splitter

--- src/Bill.java
+++ src/Bill.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
+
 public class Bill {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("how much is your bill??");
+
+        Integer getAmount = input.nextInt();
+
+        System.out.println("how many people in your party??");
+
+        Integer totalPeople = input.nextInt();
+
+        System.out.println("ok your total is " + getAmount / totalPeople);
+
     }
 
-        public static int totalAmount(int a, int b){
-       return a + b;
-    }
-
-        public static int split(int total, int people){
-        return total/people;
-    }
+//        public static int totalAmount(int a, int b){
+//       return a + b;
+//    }
+//
+//        public static int split(int total, int people){
+//        return total/people;
+//    }
 
 }
