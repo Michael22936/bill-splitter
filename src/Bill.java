d5e39ee11f66dffc5f911ca95de4b632c8ea45c7
Michael McDonnell <Michael.McDonnell067@gmail.com>
2020-01-21 11:04:08 -0600

    working on condional logic

--- src/Bill.java
+++ src/Bill.java
@@ -7,19 +7,33 @@ public class Bill {
 
         System.out.println("how much is your bill??");
 
-        Integer getAmount = input.nextInt();
+        Double getAmount = input.nextDouble();
 
         System.out.println("Do you want to add a tip??? [Y/N]");
 
         if (input.nextLine().equalsIgnoreCase("y")){
-            System.out.println("how much do you want to add");
+            System.out.println("how much do you want to add???");
+
+            Double tip = input.nextDouble();
+
+            System.out.println("how many people in your party??");
+
+            Integer totalPeople = input.nextInt();
+
+            System.out.println("ok your total is " + getAmount + tip / totalPeople);
+
         }
+
+        if (input.nextLine().equalsIgnoreCase("n")){
 
         System.out.println("how many people in your party??");
 
         Integer totalPeople = input.nextInt();
 
         System.out.println("ok your total is " + getAmount / totalPeople);
+
+        }
+
 
     }
 
