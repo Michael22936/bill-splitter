4dc2c4fe9058b70ff7e202507196ba1b56c2d3f9
Michael McDonnell <Michael.McDonnell067@gmail.com>
2020-06-17 17:08:46 -0500

    got it working but had to work around it. Need to do some more research

--- src/Bill.java
+++ src/Bill.java
@@ -11,7 +11,11 @@ public class Bill {
 
         System.out.println("Do you want to add a tip??? [Y/N]");
 
-        if (input.nextLine().equalsIgnoreCase("n")){
+        if (input.nextLine().equalsIgnoreCase("a")){
+
+            System.out.println("Wrong input");
+
+        } else if (input.nextLine().equalsIgnoreCase("y")){
 
         System.out.println("how much do you want to add???");
 
@@ -23,7 +27,7 @@ public class Bill {
 
         System.out.println("ok your total is " + (getAmount + tip) / totalPeople1);
 
-        } else if (input.nextLine().equalsIgnoreCase("y")) {
+        } else if (input.nextLine().equalsIgnoreCase("n")) {
 
         System.out.println("how many people in your party??");
 
